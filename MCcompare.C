07f54eb889967f54b2ec61538e877bbeca7eaa5f
christopher dilks <christopher.j.dilks@gmail.com>
2019-09-17 18:55:16 -0400

    update MCcompare.C

--- MCcompare.C
+++ MCcompare.C
@@ -1,7 +1,8 @@
 // compare MC generated and reconstructed distributions; use diagnostics.exe
-// to generate plots.root files for generated set and for reconstructed set:
+// to generate plots.root files for generated, reconstructed, and data sets:
 // - diagnostics.exe outroot.MC.gen 0x34 && mv plots{,.gen}.root
 // - diagnostics.exe outroot.MC.rec 0x34 && mv plots{,.rec}.root
+// - diagnostics.exe outroot.fall18.some 0x34 && mv plots{,.data}.root
 // - root MCcompare.C
 
 R__LOAD_LIBRARY(DihBsa)
@@ -12,31 +13,35 @@ R__LOAD_LIBRARY(DihBsa)
 #include "TString.h"
 #include "TMath.h"
 
-enum fenum {kGen,kRec,kRat};
+enum fenum {kGen,kRec,kData,N};
+enum renum {kRecGen=N,kRecData,NR};
 int f;
-TFile * infile[2];
+TFile * infile[N];
+Int_t plotColor[NR];
 
 TH1D * Fetch(Int_t f_,TString n_);
 void Acceptance(TH1D ** dists_);
 
 void MCcompare(TString fGen="plots.gen.root",
-               TString fRec="plots.rec.root"
+               TString fRec="plots.rec.root",
+               TString fData="plots.data.root"
 ) {
 
   infile[kGen] = new TFile(fGen,"READ");
   infile[kRec] = new TFile(fRec,"READ");
+  infile[kData] = new TFile(fData,"READ");
 
-  TH1D * WDist[3];
-  TH1D * XDist[3];
-  TH1D * MhDist[3];
-  TH1D * ZpairDist[3];
-  TH1D * xFDist[3];
-  TH1D * thetaDist[3];
-  TH1D * PhiHDist[3];
-  TH1D * PhiRDist[3];
-  TH1D * PhiHRDist[3];
+  TH1D * WDist[NR];
+  TH1D * XDist[NR];
+  TH1D * MhDist[NR];
+  TH1D * ZpairDist[NR];
+  TH1D * xFDist[NR];
+  TH1D * thetaDist[NR];
+  TH1D * PhiHDist[NR];
+  TH1D * PhiRDist[NR];
+  TH1D * PhiHRDist[NR];
 
-  for(f=0; f<2; f++) {
+  for(f=0; f<N; f++) {
     WDist[f] = Fetch(f,"WDist");
     XDist[f] = Fetch(f,"XDist");
     MhDist[f] = Fetch(f,"MhDist");
@@ -49,6 +54,12 @@ void MCcompare(TString fGen="plots.gen.root",
   };
 
   gStyle->SetOptStat(0);
+
+  plotColor[kGen] = kRed;
+  plotColor[kRec] = kCyan+1;
+  plotColor[kData] = kMagenta;
+  plotColor[kRecGen] = kBlack;
+  plotColor[kRecData] = kBlack;
 
   Acceptance(WDist);
   Acceptance(XDist);
@@ -74,32 +85,64 @@ TH1D * Fetch(Int_t f_,TString n_) {
 void Acceptance(TH1D ** dists_) {
   TString str;
 
-  dists_[kRat] = (TH1D*) dists_[kRec]->Clone();
+  dists_[kRecGen] = (TH1D*) dists_[kRec]->Clone();
+  dists_[kRecData] = (TH1D*) dists_[kRec]->Clone();
 
-  str = "(red=gen, blue=rec) ";
+  /*
+  str = "(red=gen, blue=rec, green=data) ";
   str += dists_[kGen]->GetTitle();
-  dists_[kGen]->SetTitle(str);
-  dists_[kRec]->SetTitle(str);
-  str = "(rec/gen) ";
-  str += dists_[kRat]->GetTitle();
-  dists_[kRat]->SetTitle(str);
+  for(f=0; f<N; f++) dists_[f]->SetTitle(str);
+  */
 
-  dists_[kGen]->SetLineColor(kRed);
-  dists_[kRec]->SetLineColor(kBlue);
-  dists_[kRat]->SetLineColor(kBlack);
+  str = "[ MCrec / MCgen ] -- " + TString(dists_[kRecGen]->GetTitle());
+  dists_[kRecGen]->SetTitle(str);
+  str = "[ MCrec / data ] -- " + TString(dists_[kRecData]->GetTitle());
+  dists_[kRecData]->SetTitle(str);
 
-  dists_[kRat]->Divide(dists_[kGen]);
+  for(f=0; f<NR; f++) {
+    dists_[f]->SetLineWidth(2);
+    dists_[f]->SetMarkerSize(0.7);
+    dists_[f]->SetLineColor(plotColor[f]);
+    dists_[f]->SetMarkerColor(plotColor[f]);
+    dists_[f]->SetMarkerStyle(kFullCircle);
+  };
+
+  TLegend * leg[NR];
+  for(f=0; f<NR; f++) leg[f] = new TLegend(0.6, 0.8, 0.9, 0.9);
+  leg[kRecGen]->AddEntry(dists_[kRec],"MC reconstructed","PLE");
+  leg[kRecGen]->AddEntry(dists_[kGen],"MC generated","PLE");
+  leg[kRecData]->AddEntry(dists_[kRec],"MC reconstructed","PLE");
+  leg[kRecData]->AddEntry(dists_[kData],"data","PLE");
+
+
+  dists_[kRecGen]->Divide(dists_[kGen]);
+  dists_[kRecData]->Divide(dists_[kData]);
+
   str = "canv_"+TString(dists_[kGen]->GetName());
   TCanvas * canv = new TCanvas(str,str,1000,1000);
-  canv->Divide(2,1);
+  canv->Divide(2,2);
+  Int_t d[2];
 
   canv->cd(1);
-  Int_t bigger = 
-    dists_[kGen]->GetMaximum() > dists_[kRec]->GetMaximum() ? 
-    kGen : kRec;
-  dists_[bigger]->Draw();
-  dists_[(bigger+1)%2]->Draw("same");
+  if(dists_[kRec]->GetMaximum() > dists_[kGen]->GetMaximum()) { d[0]=kRec; d[1]=kGen; }
+  else { d[0]=kGen; d[1]=kRec; };
+  dists_[d[0]]->Draw();
+  dists_[d[1]]->Draw("same");
+  leg[kRecGen]->Draw();
 
   canv->cd(2);
-  dists_[kRat]->Draw();
+  dists_[kRecGen]->Draw();
+
+  canv->cd(3);
+  if(dists_[kRec]->GetMaximum() > dists_[kData]->GetMaximum()) { d[0]=kRec; d[1]=kData; }
+  else { d[0]=kData; d[1]=kRec; };
+  dists_[d[0]]->Draw();
+  dists_[d[1]]->Draw("same");
+  leg[kRecData]->Draw();
+
+  canv->cd(4);
+  dists_[kRecData]->Draw();
+  Float_t minFit = dists_[kRecData]->GetXaxis()->GetXmin();
+  Float_t maxFit = dists_[kRecData]->GetXaxis()->GetXmax();
+  dists_[kRecData]->Fit("pol0","","",minFit,maxFit);
 };
