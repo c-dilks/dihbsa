8f995cc6a996c73f71a29f89339f4f8ba6f5a735
Christopher Dilks <dilks@ifarm1802.jlab.org>
2020-02-25 20:14:12 -0500

    more slurm scripts

---
+++ slurm.buildOutroot.pass0.rgk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=dihbsa_buildOutroot
+#SBATCH --account=clas12
+#SBATCH --partition=production
+
+#SBATCH --mem-per-cpu=3000
+#SBATCH --time=1:30:00
+
+#SBATCH --array=0-4
+#SBATCH --ntasks=1
+
+#SBATCH --output=/farm_out/%u/%x-%j-%N.out
+#SBATCH --error=/farm_out/%u/%x-%j-%N.err
+
+dataList=(/lustre19/expphy/volatile/clas12/rg-k/production/pass0/physTrain/dst/train/skim4/*)
+
+srun buildOutroot.exe ${dataList[$SLURM_ARRAY_TASK_ID]}
+
--- slurm.buildSpinroot.pass0.rgk.sh
+++ slurm.buildSpinroot.pass0.rgk.sh
@@ -7,13 +7,13 @@
 #SBATCH --mem-per-cpu=3000
 #SBATCH --time=1:30:00
 
-#SBATCH --array=0-8
+#SBATCH --array=0-4
 #SBATCH --ntasks=1
 
 #SBATCH --output=/farm_out/%u/%x-%j-%N.out
 #SBATCH --error=/farm_out/%u/%x-%j-%N.err
 
-dataList=(${PWD}/outroot/*)
+dataList=(${PWD}/outroot/*.root)
 
 srun buildSpinroot.exe -f ${dataList[$SLURM_ARRAY_TASK_ID]} -i2 -t2 -l1 -m1 -b
 
